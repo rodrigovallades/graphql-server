050a8239d799ced012c5c3a5bb20133849e9ba11
Rodrigo Vallades <rodrigo.vallades@gmail.com>
2019-03-06 17:47:21 -0300

    ✨ Add an interface to a schema

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const {
 } = require('graphql');
 
 const { getVideoById, getVideos, createVideo } = require('./src/data');
+const nodeInterface = require('./src/node');
 
 const PORT = process.env.PORT || 3000;
 const server = express();
@@ -24,7 +25,7 @@ const videoType = new GraphQLObjectType({
 	description: 'A video',
 	fields: {
 		id: {
-			type: GraphQLID,
+			type: new GraphQLNonNull(GraphQLID),
 			description: 'The ID of the video.',
 		},
 		title: {
@@ -39,8 +40,10 @@ const videoType = new GraphQLObjectType({
 			type: GraphQLBoolean,
 			description: 'Whether or not the video has been watched.',
 		},
-	}
+	},
+	interfaces: [nodeInterface],
 });
+exports.videoType = videoType;
 
 const queryType = new GraphQLObjectType({
 	name: 'QueryType',
