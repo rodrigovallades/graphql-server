954cffed9d562173ac5db2bdeea25b6d30c37da6
Rodrigo Vallades <rodrigo.vallades@gmail.com>
2019-03-06 13:25:54 -0300

    ♻️ Refactor GraphQL schema to JavaScript

--- index.js
+++ index.js
@@ -2,38 +2,62 @@
 
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const { graphql, buildSchema } = require('graphql');
+const {
+	GraphQLBoolean,
+	GraphQLID,
+	GraphQLInt,
+	GraphQLObjectType,
+	GraphQLSchema,
+	GraphQLString,
+} = require('graphql');
 
 const PORT = process.env.PORT || 3000;
 const server = express();
 
-const schema = buildSchema(`
-type Video {
-	id: ID,
-	title: String,
-	duration: Int,
-	watched: Boolean
-}
+const videoType = new GraphQLObjectType({
+	name: 'Video',
+	description: 'A video',
+	fields: {
+		id: {
+			type: GraphQLID,
+			description: 'The ID of the video.',
+		},
+		title: {
+			type: GraphQLString,
+			description: 'The title of the video.',
+		},
+		duration: {
+			type: GraphQLInt,
+			description: 'The duration of the video (in seconds).',
+		},
+		watched: {
+			type: GraphQLBoolean,
+			description: 'Whether or not the video has been watched.',
+		},
+	}
+});
 
-type Query {
-	video: Video
-	videos: [Video]
-}
+const queryType = new GraphQLObjectType({
+	name: 'QueryType',
+	description: 'The root querty type.',
+	fields: {
+		video: {
+			type: videoType,
+			resolve: () => new Promise((resolve) => {
+				resolve({
+					id: 'a',
+					title: 'GraphQL',
+					duration: 180,
+					watched: false,
+				});
+			}),
+		},
+	},
+});
 
-type Schema {
-	query: Query
-}
-`);
-
-const resolvers = {
-	video: () => ({
-		id: '1',
-		title: 'bar',
-		duration: 180,
-		watched: true,
-	}),
-	videos: () => videos,
-};
+const schema = new GraphQLSchema({
+	query: queryType,
+});
 
 const videos = [
 	{
@@ -53,7 +77,6 @@ const videos = [
 server.use('/graphql', graphqlHTTP({
 	schema,
 	graphiql: true,
-	rootValue: resolvers,
 }));
 
 server.listen(PORT, () => {
