fbb30273851f0da55fd56e89d83f8e9118875b7b
Rodrigo Vallades <rodrigo.vallades@gmail.com>
2019-03-06 18:14:11 -0300

    ♻️ Refactor GraphQL list type into a Relay connection type

--- index.js
+++ index.js
@@ -15,7 +15,12 @@ const {
 } = require('graphql');
 
 const { getVideoById, getVideos, createVideo } = require('./src/data');
-const { globalIdField } = require('graphql-relay');
+const {
+	globalIdField,
+	connectionDefinitions,
+	connectionFromPromisedArray,
+	connectionArgs,
+} = require('graphql-relay');
 const { nodeInterface, nodeField } = require('./src/node');
 
 const PORT = process.env.PORT || 3000;
@@ -43,6 +48,19 @@ const videoType = new GraphQLObjectType({
 });
 exports.videoType = videoType;
 
+const { connectionType: VideoConnection } = connectionDefinitions({
+	nodeType: videoType,
+	connectionFields: () => ({
+		totalCount: {
+			type: GraphQLInt,
+			description: 'A count of the total number of objects in this connection.',
+			resolve: (conn) => {
+				return conn.edges.length;
+			}
+		}
+	}),
+});
+
 const queryType = new GraphQLObjectType({
 	name: 'QueryType',
 	description: 'The root querty type.',
@@ -61,8 +79,12 @@ const queryType = new GraphQLObjectType({
 			},
 		},
 		videos: {
-			type: new GraphQLList(videoType),
-			resolve: getVideos,
+			type: VideoConnection,
+			args: connectionArgs,
+			resolve: (_, args) => connectionFromPromisedArray(
+				getVideos(),
+				args
+			)
 		}
 	},
 });
