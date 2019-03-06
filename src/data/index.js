f0a064297c45b2c7821b7c989d8366a91957ac4a
Rodrigo Vallades <rodrigo.vallades@gmail.com>
2019-03-06 17:26:01 -0300

    ✨ Add a mutation

--- src/data/index.js
+++ src/data/index.js
@@ -20,5 +20,19 @@ const getVideoById = (id) => new Promise((resolve) => {
 
 const getVideos = () => new Promise((resolve) => resolve(videos));
 
+const createVideo = ({ title, duration, released }) => {
+	const video = {
+		id: (new Buffer(title, 'utf8')).toString('base64'),
+		title,
+		duration,
+		released,
+	};
+
+	videos.push(video);
+
+	return video;
+}
+
 exports.getVideoById = getVideoById;
 exports.getVideos = getVideos;
+exports.createVideo = createVideo;
